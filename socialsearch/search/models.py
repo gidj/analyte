52b6a990dfb21aea1e222788e6295aee4796a478
Gideon VanRiette <gvanriette@gmail.com>
2016-09-25 17:49:28 -0500

    Add registration to API classes, clean up search models a bit

--- socialsearch/search/models.py
+++ socialsearch/search/models.py
@@ -4,18 +4,21 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
-from apps.socialapi import *
+
+import socialapi
 
 
 class SocialMediaNetwork(models.Model):
     name = models.CharField(u"Name", required=True)
-    code = models.CharField(u"Code", required=True)
-
-    #TODO: Fix this to get proper class from a registry
-    api_connection = get_api_connect(self.code)
+    api_code = models.CharField(u"Search API Code",
+        choices=socialapi.api_code_choices(),
+        blank=False)
 
     def __unicode__(self):
         return self.name
+
+    def api_connection(self):
+        return socialapi.get_api_class(self.api_code)
 
 
 class SocialSearch(models.Model):
@@ -27,7 +30,7 @@ class SocialSearch(models.Model):
 
 
 class SocialSearchExecution(models.Model):
-    social_search = models.ForeignKey(SocialSearchQuery, related_name="search")
+    social_search = models.ForeignKey(SocialSearch, related_name="search")
     # Since the original SocialSearch.query related to this may change with
     # time, we duplicate it here at the time of search execution.
     searched_query = models.CharField(u"Keyword that was Searched", blank=False)
@@ -37,27 +40,30 @@ class SocialSearchExecution(models.Model):
 
     def as_csv_file(self):
         rows = []
+        rows.append([u"Title", u"Content Link", "Social Media Source"])
         for result in self.results.all():
-            row = result.data_to_row()
-            rows.append(row)
+            rows.append(result.data_to_row)
 
         # Returns csv file object
-        return write_to_csv_file(self.filename, rows)
+        return write_to_csv_file(self._csv_filename, rows)
 
     @property
-    def csv_filename(self):
+    def _csv_filename(self):
         return "{date}_{keyword}_{sources}.csv".format(
             date=self.date.strftime(u"%Y%m%d"),
             keyword=self.searched_query,
-            sources=u"_".join(social_search.social_networks.all()))
+            sources=u"_".join(self.social_search.social_networks.all()))
+
 
 class SocialSearchResult(models.Model):
     search_execution = models.ForignKey(u"Social Search Execution",
             blank=False,
             related_name=u"results")
     title = models.CharField(u"Title", blank=False)
-    content_link models.CharField(u"URI", blank=False)
+    content_link = models.CharField(u"URI", blank=False)
     source = models.ForeignKey(SocialMediaNetwork, required=True)
 
     def data_to_row(self):
         return [self.title, self.content_link, self.source.name]
+
+
--- socialsearch/socialapi/__init__.py
+++ socialsearch/socialapi/__init__.py
@@ -2,8 +2,29 @@
     makes sense to have an abstract base class to define common methods, and then
     implement them specifically for each site in their own subclass. """
 
+
+_SOCIAL_API_MAP = {}
+_SOCIAL_API_CHOICES = []
+
+def register_social_api(api_class):
+    # This decorator registers a class as SocialAPI
+    _SOCIAL_API_MAP[api_class.key] = api_class
+    _SOCIAL_API_CHOICES.append([api_class.key, api_class.name])
+    return api_class
+
+def get_api_class(api_code):
+    return _SOCIAL_API_MAP[api_code]
+
+def api_code_choices():
+    return _SOCIAL_API_CHOICES
+
 class SocialAPIBase(object):
-    key = NotImplemented
+    """ Abstract Base Class for building searching objects for social media
+        sites. 'api_code' and 'api_name' are used for registering each search
+        API class, and in turn limiting which API classes are available for
+        performing searches. """
+    api_code = NotImplemented
+    api_name = NotImplemented
 
     class Meta:
         abstract = True
@@ -18,13 +39,18 @@ class SocialAPIBase(object):
         raise NotImplementedError
 
 
+@register_social_api
 class RedditAPI(SocialAPIBase):
     """ Interfaces with Reddit's API to search for keywords. """
-    key = u"reddit"
+    api_code = u"REDDIT"
+    api_name = u"Reddit Search API"
 
+
+@register_social_api
 class TwitterAPI(SocialAPIBase):
     """ Interface with Twitter's API to search for keywords. """
-    key = u"twitter"
+    api_code = u"TWITTER"
+    api_name = u"Twitter Search API"
 
     # Twitter's API is OAuth2 based, so we ne
     pass
